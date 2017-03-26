76f832efca335c4cd773fa9db45e441bc575b653
kundan <kundan.pes@gmail.com>
2017-03-26 14:06:14 -0300

    Updated to calculate priors.

--- src/naive_bayes/NaiveBayes.java
+++ src/naive_bayes/NaiveBayes.java
@@ -13,12 +13,19 @@ import java.util.List;
  */
 public class NaiveBayes {
 
+
 	private String SPACE=" ";
 	private String COMMA=",";
+
+	private int mTargetIdx=-1;
+
 	private boolean KEEP_DUPLICATES = true;//false;
 
 	/** The unique attributes. */
 	private List<String> mAttributes = new ArrayList<String>();
+
+	/** Stores the target class priors */
+	private HashMap<String, Double> mClassPriors = new HashMap<String, Double>();
 
 	/** The attributes to unique values map. */
 	private HashMap<String, List<String>> mAtbToUnqVals= new HashMap<String,List<String>>();
@@ -26,29 +33,75 @@ public class NaiveBayes {
 	/** The training data read from file and pre-processed.*/
 	private List<HashMap<String,String> > mTrainingData = new ArrayList<HashMap<String, String>>();
 
-	private int mTargetIdx=-1;
+	/** Holds priors for the attribute values.*/
+	private HashMap<String,HashMap<String,HashMap<String, Double>>> mTargetAtbAtbValToPrior = new HashMap<>();
 
 
-	
 	public void train(String trainDataPath, String delimiter, int targetIdx) throws IOException {
 		if(!(delimiter.equals(SPACE) || delimiter.equals(COMMA) )){
 			System.out.println("Error >> Invalid delimiter specified.");
 			return;
 		}
-		
+
 		mTargetIdx = targetIdx;
-		
+
 		/* Read training data from file.*/
 		readTrainingData(trainDataPath,delimiter);
 
 		/* Remove spurious and duplicate data*/
 		preProcessTrainingData();
 
-
+		buildClassifier();
 
 	}
 
 
+	private void buildClassifier(){
+		/* Get target class priors*/
+		HashMap<String,Integer> targetAtbValToCount = new HashMap<String,Integer> (); 
+		String targetClass= mAttributes.get(mTargetIdx);
+		for(int idx=0; idx<mAtbToUnqVals.get(targetClass).size();idx++){
+			String targetAtbVal = mAtbToUnqVals.get(targetClass).get(idx);
+			for(int cnt=0; cnt<mTrainingData.size();cnt++){
+				if(mTrainingData.get(cnt).get(targetClass).equals(targetAtbVal)){
+					Integer prevVal = 0;
+					if(targetAtbValToCount.containsKey(targetClass)){
+						prevVal = targetAtbValToCount.get(targetClass);
+					}
+					targetAtbValToCount.put(targetClass,prevVal+1);
+				}
+			}
+		}
+		for(String targetAtb:mClassPriors.keySet() ){
+			Double prior = (double) (targetAtbValToCount.get(targetAtb)/mTrainingData.size());
+			mClassPriors.put(targetAtb,prior);
+		}
+
+		/* Get class based priors for each attribute-values*/
+		for(int idx=0; idx<mAtbToUnqVals.get(targetClass).size();idx++){
+			HashMap<String,HashMap<String, Double>> atbToatbValCount = new HashMap<String,HashMap<String, Double>>();
+			String targetAtbVal = mAtbToUnqVals.get(targetClass).get(idx);
+			for(String atb:mAtbToUnqVals.keySet()){
+				if(!atb.equals(targetClass)){
+					HashMap<String, Double> atbValToCount = new HashMap<String, Double>();
+					List<String> atbVals = mAtbToUnqVals.get(atb);
+					for(int cnt=0;cnt<atbVals.size();cnt++){
+						atbValToCount.put(atbVals.get(cnt), (double) 0);
+						for(int m=0; m<mTrainingData.size();m++){
+							if( mTrainingData.get(m).get(atb).equals(atbVals.get(cnt))){
+								Double prevVal = atbValToCount.get(atbVals.get(cnt));
+								atbValToCount.put(atbVals.get(cnt), prevVal+1);
+							}
+						}
+						atbToatbValCount.put(atb, atbValToCount);
+					}
+				}
+			}
+			mTargetAtbAtbValToPrior.put(targetAtbVal, atbToatbValCount);
+		}
+	}
+	
+	
 	private void printTrainingData(int dataIdx){
 		String data="";
 		HashMap<String, String> vals= mTrainingData.get(dataIdx);
@@ -57,9 +110,10 @@ public class NaiveBayes {
 		}
 		System.out.println(data);
 	}
+
 	private void preProcessTrainingData() {
 		List<Integer> idxToRemove = new ArrayList<Integer>(); 
-		
+
 		for(int idx=0; idx<mTrainingData.size();idx++){
 			HashMap<String,String> oneAtbToVal = mTrainingData.get(idx);
 
@@ -76,14 +130,12 @@ public class NaiveBayes {
 						break;
 					}
 				}
-				
-				
 
 				/* If all attributes are same, add to drop list.*/
 				if(areAllAtbValSame){
-					System.out.println("Same attributes:"+idx+":"+idx1);
-					printTrainingData(idx);printTrainingData(idx1);System.out.println("\n");
-					
+					//System.out.println("Same attributes:"+idx+":"+idx1);
+					//printTrainingData(idx);printTrainingData(idx1);System.out.println("\n");
+
 					/* If target value of oneAtbToVal and atbToVal are different, */
 					/* drop both. If same, drop if duplicates are not allowed.    */
 					if(!oneAtbToVal.get( mAttributes.get(mTargetIdx)).
@@ -104,14 +156,18 @@ public class NaiveBayes {
 				}
 			}
 		}
-		
+
+		System.out.println("Duplicate rows:");
+		System.out.println(idxToRemove.toString());
+
 		/* Delete identified data from the list.*/
 		int shift=0;
 		for(int idx=0;idx<idxToRemove.size();idx++){
 			mTrainingData.remove(idx-shift);
 			shift=shift+1;
 		}
-		
+
+
 		System.out.println("\n\n-----FINAL TRAINING DATA--------");
 		System.out.println("Total Rows:"+mTrainingData.size());
 		for(int idx=0; idx<mTrainingData.size();idx++){
